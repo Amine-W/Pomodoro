3051c30948019c6a45cb12a55493c6eac05f4a6b
Amine Wahab <aminewahab37@gmail.com>
2023-11-16 07:00:04 +0100

    Changement de fonds et compteur de cycle

--- script.js
+++ script.js
@@ -1,62 +1,84 @@
-// Durées par défaut en secondes (25 minutes pour le travail, 5 minutes pour la pause)
 const travailDurée = 25 * 60;
 const pauseDurée = 5 * 60;
 
 let tempsRestant = travailDurée;
-let enCours = false;
+let enCours = 'travail'; 
 let timerInterval = null;
 
 var alarmSound = new Audio('sons/Radial.mp3');
 
 function démarrerTimer() {
-  if (!enCours) {
-    enCours = true;
+  if (!timerInterval) { 
     timerInterval = setInterval(miseÀJourTimer, 1000);
   }
 }
+
+function arrêterTimer() {
+  clearInterval(timerInterval);
+  timerInterval = null; // Réinitialiser timerInterval à null
+}
+
+function réinitialiserTimer() {
+  arrêterTimer();
+  tempsRestant = travailDurée;
+  enCours = 'travail'; // Réinitialiser l'état à 'travail'
+  afficherTemps(tempsRestant);
+}
+
+function afficherTemps(secondes) {
+  const display = document.getElementById('display');
+  const minutes = Math.floor(secondes / 60);
+  const secondesRestantes = secondes % 60;
+  display.textContent = `${minutes}:${secondesRestantes < 10 ? '0' : ''}${secondesRestantes}`;
+}
+
+
+
+let cyclesComplétés = 0;
 
 function miseÀJourTimer() {
   if (tempsRestant > 0) {
     tempsRestant--;
     afficherTemps(tempsRestant);
   } else {
-    alarmSound.play()
+    alarmSound.play();
     arrêterTimer();
-    // Alterner entre les périodes de travail et de pause
-    tempsRestant = (tempsRestant === travailDurée) ? pauseDurée : travailDurée;
+
+    if (enCours === 'travail') {
+      tempsRestant = pauseDurée;
+      enCours = 'pause';
+    } else {
+      tempsRestant = travailDurée;
+      enCours = 'travail';
+      cyclesComplétés++;
+      document.getElementById('completedCycles').textContent = `Cycles Complétés: ${cyclesComplétés}`;
+    }
+
+    afficherTemps(tempsRestant);
     démarrerTimer();
   }
 }
 
-function arrêterTimer() {
-  clearInterval(timerInterval);
-  enCours = false;
-}
-
-function réinitialiserTimer() {
-  arrêterTimer();
-  tempsRestant = travailDurée;
-  afficherTemps(tempsRestant);
-}
-
 function afficherTemps(secondes) {
+  const display = document.getElementById('display');
+  const progressBarInner = document.getElementById('progressBarInner');
+  const totalSecondes = enCours === 'travail' ? travailDurée : pauseDurée;
+  const progress = ((totalSecondes - secondes) / totalSecondes) * 100;
+
+  progressBarInner.style.width = `${progress}%`;
+
   const minutes = Math.floor(secondes / 60);
   const secondesRestantes = secondes % 60;
-  console.log(`${minutes}:${secondesRestantes < 10 ? '0' : ''}${secondesRestantes}`);
+  display.textContent = `${minutes}:${secondesRestantes < 10 ? '0' : ''}${secondesRestantes}`;
 }
 
+function changerThème(themeName) {
+    document.body.className = themeName;
+  }
 
-
-function afficherTemps(secondes) {
-    const display = document.getElementById('display');
-    const minutes = Math.floor(secondes / 60);
-    const secondesRestantes = secondes % 60;
-    display.textContent = `${minutes}:${secondesRestantes < 10 ? '0' : ''}${secondesRestantes}`;
-}
 
 document.getElementById('start').addEventListener('click', démarrerTimer);
 document.getElementById('stop').addEventListener('click', arrêterTimer);
 document.getElementById('reset').addEventListener('click', réinitialiserTimer);
 
-
-afficherTemps(travailDurée);
+afficherTemps(travailDurée); // Afficher le temps initial
